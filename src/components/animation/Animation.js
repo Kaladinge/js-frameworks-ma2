67afe95234377aee1d0cf6f8ace476d17e9d10d5
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2022-10-22 17:59:11 +0200

    made boy disappear and new form appear

--- src/components/animation/Animation.js
+++ src/components/animation/Animation.js
@@ -3,9 +3,19 @@ import React from "react";
 function Animation() {
   return (
     <div className="border mx-auto image-container position-relative">
-      <img className="border boy" src="/assets/boy.png" />
-      <img className="border eyes position-absolute" src="/assets/eyes.png" />
-      <img className="border mouth position-absolute" src="/assets/mouth.png" />
+      <div className="boy-container">
+        <img className="border boy" src="/assets/boy.png" />
+        <img className="border eyes position-absolute" src="/assets/eyes.png" />
+        <img
+          className="border mouth position-absolute"
+          src="/assets/mouth.png"
+        />
+        <img
+          className="border mouth2 position-absolute"
+          src="/assets/mouth2.png"
+        />
+      </div>
+      <img className="border boy3 position-absolute" src="/assets/boy3.png" />
     </div>
   );
 }
