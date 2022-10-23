d590a4d99dcde39b2c9db1ca1476e79a71d0758e
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2022-10-23 16:57:53 +0200

    added final animations

--- src/components/animation/Animation.js
+++ src/components/animation/Animation.js
@@ -2,24 +2,24 @@ import React from "react";
 
 function Animation() {
   return (
-    <div className="border mx-auto image-container position-relative">
-      <div className="boy-container">
-        <img className="border boy" src="/assets/boy.png" />
-        <img className="border eyes position-absolute" src="/assets/eyes.png" />
-        <img
-          className="border mouth position-absolute"
-          src="/assets/mouth.png"
-        />
-        <img
-          className="border mouth2 position-absolute"
-          src="/assets/mouth2.png"
-        />
+    <>
+      <div className="mx-auto image-container position-relative">
+        <div className="boy-container">
+          <img className="boy" src="/assets/boy.png" />
+          <img className="eyes position-absolute" src="/assets/eyes.png" />
+          <img className="mouth position-absolute" src="/assets/mouth.png" />
+          <img className="mouth2 position-absolute" src="/assets/mouth2.png" />
+        </div>
+        <img className="boy3 position-absolute" src="/assets/boy3.png" />
+        <div className="zombie-container position-absolute">
+          <img className="zombie" src="/assets/zombie.png" />
+        </div>
       </div>
-      <img className="border boy3 position-absolute" src="/assets/boy3.png" />
-      <div className="zombie-container border position-absolute">
-        <img className="border zombie" src="/assets/zombie.png" />
+      <div className="position-relative">
+        <img className="grass position-relative" src="/assets/grass.png" />
+        <img className="logo position-absolute" src="/assets/logo.png" />
       </div>
-    </div>
+    </>
   );
 }
 
